6e381992e46bddfee603658e5f3029cd9c55112d
Rémy Marronnier <rmarronnier@gmail.com>
2019-10-30 10:55:58 +0100

    adds administrate

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  namespace :admin do
+      resources :courses
+
+      root to: "courses#index"
+    end
   resources :courses
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
