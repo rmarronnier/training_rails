6b39c07e46744a9daa4612ab33e029731a0be041
Rémy Marronnier <rmarronnier@gmail.com>
2019-10-30 12:34:28 +0100

    routing fixes

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,11 @@ Rails.application.routes.draw do
 
   resources :users
   resources :sessions, only: [:new, :create, :destroy]  
-  
+
   get "signup", to: "users#new", as: "signup"
   get "login", to: "sessions#new", as: "login"
   get "logout", to: "sessions#destroy", as: "logout"
+  post "/sessions/new", to: "sessions#create"
 
   
   namespace :admin do
