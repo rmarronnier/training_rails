f419152eee437f3cceef844cb02910db6684f9fd
Rémy Marronnier <rmarronnier@gmail.com>
2019-10-30 11:19:08 +0100

    user + session

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,19 @@
 Rails.application.routes.draw do
-  namespace :admin do
-      resources :courses
+  root "home#index"
 
-      root to: "courses#index"
-    end
+  resources :users
+  resources :sessions, only: [:new, :create, :destroy]  
+  
+  get "signup", to: "users#new", as: "signup"
+  get "login", to: "sessions#new", as: "login"
+  get "logout", to: "sessions#destroy", as: "logout"
+
+  
+  namespace :admin do
+    resources :courses
+
+    root to: "courses#index"
+  end
   resources :courses
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
